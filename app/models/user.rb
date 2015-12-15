a8ffc2501ea8dd19f8f60a8bb30ea554cfe30595
Sheri Zada <sheri.h.zada@gmail.com>
2015-12-15 09:58:35 -0800

    user associations, remove old methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,13 @@
 class User < ActiveRecord::Base
   attr_reader :password
-  after_initialize :ensure_session_token, :ensure_default_taste
+  after_initialize :ensure_session_token
 
   validates :email, :session_token, :password_digest,
       :name, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
   validates :email, :name, :session_token, uniqueness: true
 
-  has_one :secretsnowman
-
-  def ensure_default_taste
-    self.taste ||= "\nglow in the dark yoyo,
-      \nglow in the dark socks,\na cute lunchbox,\na hug".html_safe
-  end
+  belongs_to :group, :event
 
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
