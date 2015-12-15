f4373fb1567273223f85cc06609568b362910d56
Sheri Zada <sheri.h.zada@gmail.com>
2015-12-15 09:56:48 -0800

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,43 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+  after_initialize :ensure_session_token, :ensure_default_taste
+
+  validates :email, :session_token, :password_digest,
+      :name, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :email, :name, :session_token, uniqueness: true
+
+  has_one :secretsnowman
+
+  def ensure_default_taste
+    self.taste ||= "\nglow in the dark yoyo,
+      \nglow in the dark socks,\na cute lunchbox,\na hug".html_safe
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    return nil if user.nil?
+    user && user.valid_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
 end
