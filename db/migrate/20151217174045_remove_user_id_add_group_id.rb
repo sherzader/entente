3b12d86938d6eb2bd315acc8ed28796c98fcb706
Sheri Zada <sheri.h.zada@gmail.com>
2015-12-21 17:41:49 -0800

    migration

--- db/migrate/20151217174045_remove_user_id_add_group_id.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdAddGroupId < ActiveRecord::Migration
-  def change
-    remove_column :groups, :event_id
-    remove_column :events, :user_id
-    add_column :events, :group_id
-    add_index :events, :group_id
-    add_index :users, :event_id
-  end
-end
