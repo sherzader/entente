b6e67a93d4f137c9f932aa96799ddca083b15637
Sheri Zada <sheri.h.zada@gmail.com>
2015-12-22 23:49:07 -0800

    styling, new modal

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,21 +1,39 @@
 var React = require('react');
 var GroupForm = require('./groupForm.jsx');
 var Link = require('react-router').Link;
-var Modal = require('boron/OutlineModal');
+var Modal = require('react-modal');
 var History = require('react-router').History;
 var ApiUtil = require('../util/apiUtil');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 var App = React.createClass({
   mixins: [History],
 
-  componentDidMount: function () {
-    this.showModal();
+  getInitialState: function() {
+    return { modalIsOpen: false };
   },
-  showModal: function(){
-    this.refs.modal.show();
-   },
-  hideModal: function(){
-    this.refs.modal.hide();
+
+  componentDidMount: function () {
+    this.setState({modalIsOpen: true});
+  },
+
+  openModal: function() {
+    this.setState({modalIsOpen: true});
+  },
+
+  closeModal: function() {
+    this.setState({modalIsOpen: false});
   },
   _logout: function () {
     ApiUtil.logout();
@@ -23,12 +41,14 @@ var App = React.createClass({
   render: function () {
     return(
       <div className="app">
-        <Modal ref="modal" className="welcome-modal">
-          <h2>Welcome, {window.CURRENT_USER.name}</h2><br />
-          Entente is a place to find groups of individuals with similar interests.<br />
-          With these kindred clans, your aim is to coordinate rituals of your choosing. <br />
-        Have fun! <br />
-        </Modal>
+          <Modal isOpen={this.state.modalIsOpen}
+            onRequestClose={this.closeModal}
+            style={customStyles} id="welcome">
+              <h2>Welcome, {window.CURRENT_USER.name}</h2><br />
+              Entente is a place to find groups of individuals with similar interests.<br />
+              With these kindred clans, your aim is to coordinate rituals of your choosing. <br />
+              Have fun! <br />
+          </Modal>
         <nav className="navbar navbar-default">
         <div className="container-fluid">
           <div className="navbar-header">
@@ -43,19 +63,18 @@ var App = React.createClass({
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav">
               <li><a href="#"></a></li>
-              <li className="group-modal" data-toggle="modal" data-target="#new-group-modal"><dt>Start Group</dt></li>
             </ul>
             <ul className="nav navbar-nav navbar-right">
-              <li><a href="#"></a></li>
-                <li className="dropdown">
-                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><dt>Profile</dt><span className="caret"></span></a>
-                  <ul className="dropdown-menu">
-                    <li><Link to={'/profile'}>My Profile</Link></li>
-                    <li role="separator" className="divider"></li>
-                    <li><a href="#" onClick={this._logout}>Logout</a></li>
-                    <li role="separator" className="divider"></li>
-                  </ul>
-                </li>
+              <li className="start-group" data-toggle="modal" data-target="#new-group-modal"><a href="#"><dt>Start Group</dt></a></li>
+              <li className="dropdown">
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><dt>Profile</dt><span className="caret"></span></a>
+                <ul className="dropdown-menu">
+                  <li><Link to={'/profile'}>My Profile</Link></li>
+                  <li role="separator" className="divider"></li>
+                  <li><a href="#" onClick={this._logout}>Logout</a></li>
+                  <li role="separator" className="divider"></li>
+                </ul>
+              </li>
             </ul>
             </div>
           </div>
