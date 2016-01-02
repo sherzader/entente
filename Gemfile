eff936e3a9663a20570172d0a10d0ca9a551c44d
Sheri Zada <sheri.h.zada@gmail.com>
2016-01-01 21:47:14 -0800

    add secrets

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'rename'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,38 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-150.times do
-  user = User.create({
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    password: "password",
-    group_id: Faker::Number.between(1, 50)
-  })
-end
-
-50.times do
-  organizer = Organizer.create({
-    user_id: Faker::Number.between(1, 150)
-    })
-end
-
-50.times do
-  group = Group.create({
-    title: Faker::Company.buzzword,
-    body: Faker::Company.catch_phrase,
-    location: "San Francisco",
-    organizer_id: Faker::Number.between(1, 150)
-    })
-end
-
-50.times do
-  event = Event.create({
-    title: Faker::Company.buzzword,
-    body: Faker::Company.bs,
-    location: Faker::Address.street_name,
-    date: Faker::Date.forward(10),
-    organizer_id: Faker::Number.between(1, 150),
-    group_id: Faker::Number.between(1, 50)
-    })
-end
