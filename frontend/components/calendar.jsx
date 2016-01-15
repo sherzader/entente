40a55e622f290714f8ce48db786757ee7908ad58
Sheri Zada <sheri.h.zada@gmail.com>
2016-01-14 19:07:23 -0800

    splash page styling

--- frontend/components/calendar.jsx
+++ frontend/components/calendar.jsx
@@ -93,7 +93,7 @@ var Calendar = React.createClass({
                 }}
                 onDayClick={ this.handleDayClick }
               />
-            <li className="calendar-caption-bottom" onClick={this._refresh}>Today</li>
+            <li className="calendar-caption-bottom" onClick={this._refresh}>Reset to Today</li>
         <GroupIndex history={this.history} groups={this.filterGroups()} />
       </div>
     );
